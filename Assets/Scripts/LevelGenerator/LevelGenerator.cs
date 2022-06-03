85260feeb22c31a09e7eb7358fa9a9ca55695bc1
Ivan <39524682+Skrals@users.noreply.github.com>
2022-06-03 08:34:49 +0300

    Шаблон генератора уровней
    Структурирование проекта и настройка спавнера

--- Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -1,8 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    
+    [SerializeField] private Ground[] _groundsTemplates;
+    [SerializeField] private int _groundsAmount;
+
+    private void Start()
+    {
+        GenerateLevel(_groundsTemplates,_groundsAmount);
+    }
+
+    private void GenerateLevel(Ground[] groundsArray, int amount )
+    {
+
+    }
+
+    private void GroundSpawn(Ground ground)
+    {
+        
+    }
 }
